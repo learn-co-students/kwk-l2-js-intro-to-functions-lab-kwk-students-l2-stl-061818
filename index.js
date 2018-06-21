ca0b8c99f175a2473abcc641703a3ddaa6471dc4
Scarlett Patton <scarwars05@gmail.com>
2018-06-21 13:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+
+function getUKTime(number) {
+  const currentHour = new Date().getHours() + 6;
+  return currentHour;
+}
+
+function discountTicket(oldPrice) {
+  const newPrice = oldPrice * 0.8;
+  return newPrice;
+}
+
+function tweetReply(fansName) {
+  const reply = `Hey ${fansName}, thanks so much for the support! I really appreciate it!`;
+  return reply;
+}
