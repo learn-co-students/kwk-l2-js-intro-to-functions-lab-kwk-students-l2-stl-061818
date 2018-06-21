56081e930da0497926ac6021b67d9e3594aa0c5c
abbylee1 <ablee@micds.org>
2018-06-21 15:00:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+function getUKTime() {
+  let UKTime = new Date().getHours() + 5
+  return UKTime
+}
+
+function discountTicket(originalTicket) {
+  return originalTicket * 0.8
+}
+
+function tweetReply(fansName) {
+  let greeting = `Hey ${fansName}, thanks so much for the support! I really appreciate it!`
+  return greeting;
+}
