8687d3a6018d079c0dbbc5a322e6fa27bc7bad5a
MiaRene15 <kids.hines@icloud.com>
2018-06-24 15:13:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+
+let currentHour = new Date().getHours()
+
+function getUKTime() {
+  return currentHour + 5
+}
+
+let percentage = 80
+
+function discountTicket(number) {
+    return (number/100)*percentage
+}
+
+function tweetReply(fanName) {
+  return `Hey ${fanName}, thanks so much for the support! I really appreciate it!`
+}
